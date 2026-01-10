6a9527b686995474c8fa42586b5c694f5ce4a3d9
Tarek Mostafa <eliteconsultant@gmail.com>
2026-01-10 12:04:53 +0200

    Update dashboard UI - Vercel test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="max-w-2xl mx-auto space-y-6">
         <div className="flex justify-between items-center">
           <h1 className="text-4xl font-bold tracking-tight">
-            Dar Al-Muzadat INTERNATIONAL 🚀
+            Dar Al-Muzadat 🚀
           </h1>
           <ModeToggle />
         </div>
